e09cd7043b0fe7c023456f0e9655e97baad903dd
Thomas Michael <thomas.michael@god.de>
2017-12-27 22:34:41 +0100

    schreibfehler

--- src/app/linux-command/drop-component/linux-command-drop.component.ts
+++ src/app/linux-command/drop-component/linux-command-drop.component.ts
@@ -17,12 +17,7 @@ export class LinuxCommandDropComponent implements OnInit, OnDestroy {
 
     transferDataSuccess($event: any) {
         let x = $event.dragData;
-        console.log('Drop', $event);
-        let keys = Object.keys(GrepOptions);
-        let grepOptionsAsString = keys.slice(keys.length / 2)
-        let xx: LinuxCommand = new LinuxCommand('grep', grepOptionsAsString);
-        console.log('xx', xx);
-        this.receivedData.push(xx);
+        this.receivedData.push(this.dispatchCommand(x));
     }
 
     constructor() { }
@@ -48,7 +43,7 @@ export class LinuxCommandDropComponent implements OnInit, OnDestroy {
                 return new LinuxCommand('wc', wcOptionsAsString);
             case 'sed':
                 let sedkeys = Object.keys(WcOptions);
-                let sedOptionsAsString = wckeys.slice(sedkeys.length / 2)
+                let sedOptionsAsString = sedkeys.slice(sedkeys.length / 2)
                 return new LinuxCommand('not Implemented', wcOptionsAsString);
 
             default:
